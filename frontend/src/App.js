e085a6b8baf035075217e87fbc8292a4268eb13b
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-11-01 22:40:31 +0100

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 
 import React from 'react';
-import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,6 +3,13 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="col text-center py-3">
+           Copyright &copy; shopYoussef
+                    </div>
+                </div>
+            </div>
             footer
         </footer>
     )
