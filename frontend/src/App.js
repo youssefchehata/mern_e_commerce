df4e174ba677707cd675cc91de581291a36ff0c9
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-11-01 22:09:12 +0100

    install bootstarap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+
 const App = () => {
   return (
     <>
       <Header />
       <main>
-        <h1>Welcome To Proshop</h1>
+        <div className="container">
+          <h1>Welcome To Proshop</h1> 
+          
+        </div>
+       
+      
       </main>
       <Footer />
     </>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,9 @@
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 
 import App from './App';
 
