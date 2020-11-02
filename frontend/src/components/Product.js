718786d81c60af73d27cdb918c61c4bf60afcc9b
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-11-02 22:46:49 +0100

    product

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-
-const Product = ({product}) => {
-    return (
-        <div>
-              <div className='col'>{product.name}</div>
+const Product = ({ product }) => {
+  return (
+    <>
+      <div className='card my-3 p-3 rounded' style={{ width: '18rem' }}>
+        <img
+          className='card-img-top'
+          src={product.image}
+          alt='Card image cap'
+        />
+        <div className='card-body'>
+          <h5 className='card-title'>{product.name}</h5>
+          <p className='card-text'>{product.rating}</p>
+          <h3>{product.price}</h3>
+          <a href='#' className='btn btn-primary'>
+            Go somewhere
+          </a>
         </div>
-    )
+      </div>
+    </>
+  );
+};
+{
+  /* <div className='col'>{product.name}</div> */
 }
 
-
-export default Product
+export default Product;
