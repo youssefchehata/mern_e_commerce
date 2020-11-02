1901d784ff4c96217edd058ecc5a3e837c1de0cd
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-11-02 22:17:37 +0100

    save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className="py-3">
         <Container>
         <h1>Welcome To Proshop</h1> 
         </Container>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,30 +1,38 @@
 // rafce
-import React from 'react'
-
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 const Header = () => {
-    return (
-        <header>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand " href="/">YoussefShop</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
+  return (
+    <header>
+      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+        <a className='navbar-brand ' href='/'>
+          Youssef_Shop
+        </a>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon' />
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-       
-            <li className="nav-item">
-              <a className="nav-link" href="#">Cart</a>
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav ml-auto'>
+            <li className='nav-item'>
+              <a className='nav-link ml-auto' href='/login'> <i className='fas fa-shopping-cart'></i> Cart </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Sign In</a>
+            <li className='nav-item'>
+              <a className='nav-link ml-auto' href='/profile'> <i className='fas fa-user'></i> Sign In </a>
             </li>
           </ul>
-
         </div>
       </nav>
-        </header>
-    )
-}
+    </header>
+  );
+};
 
-export default Header
-
+export default Header;
