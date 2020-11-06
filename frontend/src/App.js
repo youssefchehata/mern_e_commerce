f74430795446d49019b5ff0a8e8f6afcb16a5ea3
youssef chehata <youssef.chehata@dotit-medu.com>
2020-11-06 11:42:51 +0100

    save fix app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,19 @@
-// import React from 'react';
-// import Footer from './components/Footer';
-// import Header from './components/Header';
-// import { Route, Switch, Redirect } from "react-router-dom";
-// import { Container } from 'react-bootstrap';
-// import HomeScreen from './screens/HomeScreen';
-// import Cart from './screens/Cart'
-// const App = (props) => {
-//   return (
-//     <Switch>
-//       <Header />
-//       <main className='py-3'>
-      
-//         <Container>
-//           <HomeScreen />
-//           <Route path="/Cart" component={Cart} />
-//           {props.children}
-//         </Container>
-
-//       </main>
-//       <Footer />
-//     </Switch>
-//   );
-// };
-
-// export default App;
 import React, { Component } from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
-import Cart from "./screens/Cart";
 import Header from './components/Header'
-import HomeScreen from '../src/screens/HomeScreen'
-import ProductDetails from '../src/screens/ProductDetails'
+import Footer from './components/Footer';
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <>
         <Header />
-        <div className="content py-3">
+        <main className='py-3'>
+        <div className="content ">
         {this.props.children}
         </div>
-      </div>
+        </main>
+        <Footer />
+      </>
     );
   }
 }
