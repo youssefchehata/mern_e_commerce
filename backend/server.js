53c0b79257b7eab5cd0077af2f73ac8af3a941cb
youssef chehata <youssef.chehata@dotit-medu.com>
2020-11-09 11:14:41 +0100

    get single product with static data in express

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const products = require('./data/products')
+const express = require('express');
+const products = require('./data/products');
 
-const app = express()
+const app = express();
 
-app.get('/api/products',(req,res)=>{
-    res.json(products)
-})
+app.get('/', (req, res) => {
+  res.send('Api is running');
+});
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(5000,console.log('server running in port 5000'))
+app.listen(5000, console.log('server running in port 5000'));
