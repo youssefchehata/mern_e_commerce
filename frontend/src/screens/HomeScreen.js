f4e37f6f535acdb55b1246289d8b5079087fb996
youssef chehata <youssef.chehata@dotit-medu.com>
2020-11-09 11:35:59 +0100

    connect react with express static data proxy axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from '../components/Product';
-import products from './../products';
+import axios from 'axios'
+
 const HomeScreen = () => {
+  const [Products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
       <div className='row'>
-        {products.map((product) => (
-        <Product product={product} key={product._id} />
+        {Products.map((product) => (
+          <Product product={product} key={product._id} />
         ))}
       </div>
     </>
