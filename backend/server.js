aa441f9483657c6b50e4d59007290f83c985b883
youssef chehata <youssef.chehata@dotit-medu.com>
2020-11-09 11:09:24 +0100

    get static data in express server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const products = require('./data/products')
 
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send('Api is running')
+app.get('/api/products',(req,res)=>{
+    res.json(products)
 })
 
 app.listen(5000,console.log('server running in port 5000'))
