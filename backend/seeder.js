627d8e14739f1699a1fd5271b6392279ef470b15
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-12-11 10:50:20 +0100

    import&destroy data

--- backend/seeder.js
+++ backend/seeder.js
@@ -6,24 +6,62 @@ import products from './data/products.js';
 import User from './models/userModel.js';
 import Product from './models/productModel.js';
 import Order from './models/orderModel.js';
-import connectedDB from './config/db.js'
-
+import connectDB from './config/db.js';
 
 dotenv.config();
 
-connectDB()
+connectDB();
+
+const importData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUsers = await User.insertMany(users);
+    const adminUser = createdUsers[0]._id
+    const sampleProducts= products.map(product=>{
+        return {...product , user:adminUser}
+    })
+   await Product.insertMany(sampleProducts)
+
+   console.log('data imported'.green.inverse);
+   process.exit()
+
+  } catch (error) {
+  console.error(`${error}`.red.inverse)
+  process.exit(1)
+
+  }
+};
 
 
-const importData = async ()=>{
+const destroyData = async () => {
     try {
-        await     Order.deleteMany()
-        await     Product.deleteMany()
-        await     User.deleteMany()
+      await Order.deleteMany();
+      await Product.deleteMany();
+      await User.deleteMany();
 
-
-    const createdUser =    await    User.insertMany(users)
-    
+  
+  
+     console.log('data Destroyed'.red.inverse);
+     process.exit()
+  
     } catch (error) {
-        
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  
     }
-}
+  };
+
+
+  if(process.argv[2]=== '-d'){
+      destroyData()
+  }else{
+      importData()
+  }
+
+
+  // node backend/seeder.js -d
+//   npm run data:import
+//   npm run data:destroy
