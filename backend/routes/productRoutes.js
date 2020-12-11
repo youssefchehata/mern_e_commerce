e12988fe3e34fa0fae84eb109212e98d37587a28
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-12-11 16:08:23 +0100

    fetsh data from data base in backend

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+const router = express.Router();
+import Product from '../models/productModel.js';
+
+// @ fetsh all products
+// @ GET  api/products
+// @ Public
+
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    res.json(products);
+  })
+);
+
+// @ fetsh single product
+// @ GET  api/products/:id
+// @ Public
+router.get(
+  '/:id',
+
+  asyncHandler(async (req, res) => {
+    try {
+      const product = await Product.findById(req.params.id);
+      res.json(product);
+    } catch (error) {
+      res.status(404).send({ error: 'Product not found' });
+    }
+  })
+);
+
+export default router;
+// const product = await Product.findById(req.params.id);
+
+// if (product) {
+//   res.json(product);
+// } else {
+//   res.status(404).send({ message: 'Product not found' });
+// }
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
-// const express = require('express');
 import express from 'express'
-// const dotenv = require('dotenv');
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-// const products = require('./data/products');
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
+
 
 dotenv.config();
 
@@ -13,15 +11,9 @@ connectDB()
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Api is running....');
-});
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.get('/', (req, res) => { res.send('Api is running....'); });
+
+app.use('/api/products',productRoutes)
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`server running ${process.env.NODE_ENV} mode in port ${PORT}`.yellow.bold));
