8f35974a1049b7ec5f6739a9e794fe4ee352e6b0
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-12-15 16:38:08 +0100

    change function from router to controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,37 @@
+import asyncHandler from 'express-async-handler';
+
+import Product from '../models/productModel.js';
+
+// @ fetsh all products
+// @ GET  api/products
+// @ Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  // res.status(401)
+  // throw new Error('bnjkldbfhcdbfjl')
+  res.json(products);
+});
+
+// @ fetsh single product
+// @ GET  api/products/:id
+// @ Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product Not found');
+    //   .send({ message: 'Product not found' });
+  }
+});
+
+export { getProducts, getProductById };
+// const product = await Product.findById(req.params.id);
+
+// if (product) {
+//   res.json(product);
+// } else {
+//   res.status(404).send({ message: 'Product not found' });
+// }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,48 +1,12 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
-
 const router = express.Router();
-import Product from '../models/productModel.js';
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productController.js';
 
-// @ fetsh all products
-// @ GET  api/products
-// @ Public
+router.route('/').get(getProducts);
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-  // res.status(401)
-  // throw new Error('bnjkldbfhcdbfjl')
-    res.json(products);
-  })
-);
-
-// @ fetsh single product
-// @ GET  api/products/:id
-// @ Public
-router.get(
-  '/:id',
-
-  asyncHandler(async (req, res) => {
-const product = await Product.findById(req.params.id);
-
-if (product) {
-  res.json(product);
-} else {
-res.status(404)
-throw new Error('Product Not found')
-//   .send({ message: 'Product not found' });
-
-}
-  })
-);
+router.route('/:id').get(getProductById);
 
 export default router;
-// const product = await Product.findById(req.params.id);
-
-// if (product) {
-//   res.json(product);
-// } else {
-//   res.status(404).send({ message: 'Product not found' });
-// }
