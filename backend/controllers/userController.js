48e1b5552756f45aaf7fb17137019f295e5c485f
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-12-15 17:58:30 +0100

    save

---
+++ backend/controllers/userController.js
@@ -0,0 +1,10 @@
+import asyncHandler from 'express-async-handler';
+
+import User from '../models/userModel.js';
+
+// @ fetsh Auth user & get token
+// @ POST  api/users/login
+// @ access Public
+const authUser = asyncHandler(async (req, res) => {
+
+});
