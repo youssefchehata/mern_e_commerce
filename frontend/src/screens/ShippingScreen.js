641b03684d03b9c2005e0a828b039328b8fd1250
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-12-18 17:48:23 +0100

    save checkoutstep

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -26,7 +26,7 @@ const submitHandler =(e)=>{
 }
   return (
     <FormContainer>
-    <CheckoutSteps step1 step2 step3 step4/>
+    <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='address'>
