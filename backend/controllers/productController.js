a24680d14bbe797dfff776703cadd51c895ffe9f
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-12-25 11:48:45 +0100

    save

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -51,7 +51,7 @@ const createProduct = asyncHandler(async (req, res) => {
     name: 'Sample name',
     price: 0,
     user: req.user._id,
-    image: '/image/sample.jpg',
+    image: '/images/sample.jpg',
     brand: 'Sample brand',
     category: 'Sample category',
     countInStock: 0,
