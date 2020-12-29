8d28c22bcb6413c35fe732d432c7499b26ccd9b9
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-12-29 09:52:02 +0100

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,6 @@ app.use(express.json());
 //Enable cors
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Api is running....');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -38,9 +35,22 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+
+
+
 //static folder
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname , '/frontend/build')))
+  app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname, 'frontend' , 'build' ,'index.html' )))
+}else{
+  app.get('/', (req, res) => { res.send('Api is running....'); });
+}
+
 
 
 //custom error handler in middleware
